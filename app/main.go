82df3c0a667ebd14099a3d0730c7b7a7ace77f7d
Adithya11811 <adithya11811@gmail.com>
2025-06-15 15:30:00 +0530

    feat:basic-setup-6

--- app/main.go
+++ app/main.go
@@ -4,58 +4,90 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
-var Keywords = map[string]bool{
-	"exit": true,
-	"echo": true,
-	"type": true,
-}
+var builtIns = []string{"type", "echo", "exit"}
+
 
 func main() {
-	run()
-}
-
-func run() {
-		// Wait for user input
-	for true{
+	for {
 		fmt.Fprint(os.Stdout, "$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	if(err != nil) {
-		fmt.Fprintln(os.Stderr, "Error reading command:", err)
-		os.Exit(1)
-	}
-
-	commands := strings.Fields(command)
-
-    if len(commands) == 2 {
-        if commands[0] == "exit" && commands[1] == "0" {
-            os.Exit(0)
-        }
-    }
-
-	if len(commands) > 0 && commands[0] == "echo" {
-		// Print the command arguments
-		fmt.Println(strings.Join(commands[1:], " "))
-		continue
-	}
-
-	if len(commands) > 0 && commands[0] == "type" {
-		// Print the type of the command
-		if _, exists := Keywords[commands[1]]; exists {
-			fmt.Println(commands[1] + " is a shell builtin")
-			continue
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
 		}
 
-		fmt.Println(strings.Join(commands[1:], " ") + ": not found")
-		continue
+		argv := strings.Fields(input)
+		cmd := argv[0]
+
+		switch cmd {
+		case "exit":
+			ExitCommand(argv)
+		case "echo":
+			EchoCommand(argv)
+		case "type":
+			TypeCommand(argv)
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+		}
+	}
+}
+
+func ExitCommand(argv []string) {
+	code := 0
+
+	if len(argv) > 1 {
+		argCode, err := strconv.Atoi(argv[1])
+		if err != nil {
+			code = argCode
+		}
 	}
 
-	fmt.Println(strings.TrimSpace(command) + ": command not found")
+	os.Exit(code)
+}
+
+func EchoCommand(argv []string) {
+	output := strings.Join(argv[1:], " ")
+	fmt.Fprintf(os.Stdout, "%s\n", output)
+}
+
+func TypeCommand(argv []string) {
+	if len(argv) == 1 {
+		return
 	}
+
+	value := argv[1]
+
+
+	if slices.Contains(builtIns, value) {
+		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", value)
+		return
+	}
+
+	if file, exists := findBinInPath(value); exists {
+		fmt.Fprintf(os.Stdout, "%s is %s\n", value, file)
+		return
+	}
+
+	fmt.Fprintf(os.Stdout, "%s: not found\n", value)
+}
+
+func findBinInPath(bin string) (string, bool) {
+	paths := os.Getenv("PATH")
+	for _, path := range strings.Split(paths, ":") {
+		file := path + "/" + bin
+		if _, err := os.Stat(file); err == nil {
+			return file, true
+		}
+	}
+
+	return "", false
 }
