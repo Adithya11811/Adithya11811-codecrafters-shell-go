4247403b47c535e98276443299dc159c02db0741
Adithya11811 <adithya11811@gmail.com>
2025-06-29 23:10:30 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	// "bufio"
-	// "bufio"
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"slices"
-	"strconv"
 	"strings"
-
-	// "syscall"
 	"unicode"
-
-	"github.com/cosiner/argv"
-	// "golang.org/x/term"
-	// "syscall"
-	// "github.com/google/shlex"
 )
 
 var histFile string
@@ -90,7 +78,7 @@ func main() {
 	hist = History{}
 	histFile = os.Getenv("HISTFILE")
 	argv := "history -r " + histFile
-	HistoryCommand(strings.Split(argv, " "), &hist)
+	HistoryCommand(strings.Split(argv, " "), os.Stdin, os.Stdout, &hist)
 	trie = NewTrie()
 
 	for i := 0; i < len(builtIns); i++ {
@@ -125,265 +113,44 @@ func main() {
 
 	}
 }
-func Menu(cmd string, argv[] string) {
-			switch cmd {
-		case "exit":
-			ExitCommand(argv, &hist)
-		case "echo":
-			EchoCommand(argv)
-		case "type":
-			TypeCommand(argv)
-		case "pwd":
-			getCurrentDir()
-		case "cd":
-			if len(argv) < 2 {
-				changeDir(os.Getenv("HOME")) // Default to HOME if no argument is provided
-			} else {
-				changeDir(argv[1])
-			}
-		case "history":
-			HistoryCommand(argv, &hist)
-			return
-		default:
-			filePath, exists := findBinInPath(cmd)
-			if exists {
-				var command *exec.Cmd
-				if len(argv) == 0 {
-					command = exec.Command(filePath)
-					command.Args = []string{cmd}
-				} else {
-					command = exec.Command(filePath, argv[1:]...)
-					command.Args = append([]string{cmd}, argv[1:]...)
-				}
-				command.Stdout = os.Stdout
-				command.Stderr = os.Stderr
-				if err := command.Run(); err != nil {
-					fmt.Fprintf(os.Stderr, "%s: %s\n", cmd, err)
-				}
-			} else {
-				fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd)
-			}
+func Menu(cmd string, argv []string) {
+	switch cmd {
+	case "exit":
+		ExitCommand(argv, os.Stdin, os.Stdout, &hist)
+	case "echo":
+		EchoCommand(argv, os.Stdin, os.Stdout)
+	case "type":
+		TypeCommand(argv, os.Stdin, os.Stdout)
+	case "pwd":
+		getCurrentDir(argv, os.Stdin, os.Stdout)
+	case "cd":
+		if len(argv) < 2 {
+			changeDir([]string{"cd", os.Getenv("HOME")}, os.Stdin, os.Stdout) // Default to HOME if no argument is provided
+		} else {
+			changeDir(argv, os.Stdin, os.Stdout)
 		}
-}
-
-func ExitCommand(argv []string, hist *History) {
-	code := 0
-
-	if len(argv) > 1 {
-		argCode, err := strconv.Atoi(argv[1])
-		if err == nil { // Only set code if conversion is successful
-			code = argCode
-		}
-	}
-
-	temp := "history -w " + histFile
-	HistoryCommand(strings.Split(temp, " "), hist)
-
-	os.Exit(code)
-}
-
-func EchoCommand(argv []string) {
-	if len(argv) < 2 {
-		fmt.Fprintln(os.Stdout, "")
+	case "history":
+		HistoryCommand(argv, os.Stdin, os.Stdout, &hist)
 		return
-	}
-	output := strings.Join(argv[1:], " ")
-	fmt.Fprintf(os.Stdout, "%s\n", output)
-}
-
-func TypeCommand(argv []string) {
-	if len(argv) == 1 {
-		return
-	}
-
-	value := argv[1]
-
-	if slices.Contains(builtIns, value) {
-		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", value)
-		return
-	}
-
-	if file, exists := findBinInPath(value); exists {
-		fmt.Fprintf(os.Stdout, "%s is %s\n", value, file)
-		return
-	}
-
-	fmt.Fprintf(os.Stdout, "%s: not found\n", value)
-}
-
-func findBinInPath(bin string) (string, bool) {
-	paths := os.Getenv("PATH")
-	for _, path := range strings.Split(paths, ":") {
-		file := filepath.Join(path, bin)
-		info, err := os.Stat(file)
-		if err == nil && !info.IsDir() && info.Mode()&0111 != 0 {
-			return file, true
-		}
-	}
-	return "", false
-}
-
-func getCurrentDir() {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting current directory: %s\n", err)
-
-	}
-	fmt.Fprintf(os.Stdout, "%s\n", currentDir)
-}
-
-func changeDir(path string) {
-	if path == "~" || path == "$HOME" {
-		path = os.Getenv("HOME")
-	}
-	if err := os.Chdir(path); err != nil {
-		fmt.Fprintf(os.Stderr, "cd: %s: No such file or directory\n", path)
-	}
-}
-
-func splitWithQuoting(inputString string) (string, []string) {
-	var current strings.Builder
-	args := []string{}
-	inSingleQuote := false
-	inDoubleQuote := false
-	escaped := false
-
-	for _, c := range inputString {
-		switch {
-		case escaped:
-			if inDoubleQuote {
-				switch c {
-				case '"', '\\', '$', '`':
-					current.WriteRune(c)
-				default:
-					current.WriteRune('\\')
-					current.WriteRune(c)
-				}
-			} else if !inSingleQuote {
-				current.WriteRune(c)
+	default:
+		filePath, exists := findBinInPath(cmd)
+		if exists {
+			var command *exec.Cmd
+			if len(argv) == 0 {
+				command = exec.Command(filePath)
+				command.Args = []string{cmd}
 			} else {
-				current.WriteRune('\\')
-				current.WriteRune(c)
+				command = exec.Command(filePath, argv[1:]...)
+				command.Args = append([]string{cmd}, argv[1:]...)
 			}
-			escaped = false
-		case c == '\\' && !inSingleQuote:
-			escaped = true
-		case c == '\'' && !inDoubleQuote:
-			inSingleQuote = !inSingleQuote
-		case c == '"' && !inSingleQuote:
-			inDoubleQuote = !inDoubleQuote
-		case unicode.IsSpace(c) && !inSingleQuote && !inDoubleQuote:
-			if current.Len() > 0 {
-				args = append(args, current.String())
-				current.Reset()
+			command.Stdin = os.Stdin
+			command.Stdout = os.Stdout
+			command.Stderr = os.Stderr
+			if err := command.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s\n", cmd, err)
 			}
-		default:
-			current.WriteRune(c)
-		}
-	}
-
-	if escaped {
-		current.WriteRune('\\')
-	}
-	if current.Len() > 0 {
-		args = append(args, current.String())
-	}
-
-	// ✅ Edge case fix
-	if len(args) == 0 {
-		return "", []string{}
-	}
-
-	return args[0], args
-}
-
-func HistoryCommand(argv []string, hist *History) {
-	cnt := 0
-
-	if len(argv) > 1 {
-		argCode, err := strconv.Atoi(argv[1])
-		if err == nil { // Only set code if conversion is successful
-			cnt = argCode
-		}
-	}
-	// fmt.Printf("%v %T\n", argv, argv)
-	if len(argv) > 2 {
-		if argv[1] == "-r" {
-			//check if argv[2] file exists
-			if _, err := os.Stat(argv[2]); err == nil && !os.IsNotExist(err) {
-				file, err := os.ReadFile(argv[2])
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error reading history file: %s\n", err)
-					return
-				}
-
-				lines := strings.Split(string(file), "\n")
-				for _, line := range lines {
-					if line != "" {
-						hist.Add(line)
-					}
-				}
-				return // <-- Only load history, do not print anything
-			} else {
-				// fmt.Fprintf(os.Stderr, "History file %s does not exist.\n", argv[2])
-				return
-			}
-		}
-		if argv[1] == "-w" {
-			if len(argv) < 3 {
-				fmt.Fprintln(os.Stderr, "Usage: history -w <filename>")
-				return
-			}
-			file, err := os.Create(argv[2])
-			if err != nil {
-				return
-			}
-			defer file.Close()
-
-			for i := 1; i <= hist.Len(); i++ {
-				if command, exists := hist.Get(i - 1); exists {
-					fmt.Fprintf(file, "%s\n", command)
-				}
-			}
-
-			return
-		}
-
-		if argv[1] == "-a" {
-			if len(argv) < 3 {
-				fmt.Fprintln(os.Stderr, "Usage: history -a <filename>")
-				return
-			}
-			file, err := os.OpenFile(argv[2], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error opening history file: %s\n", err)
-				return
-			}
-			defer file.Close()
-
-			for i := hist.lastAppendedIndex; i < hist.Len(); i++ {
-				if command, exists := hist.Get(i); exists {
-					fmt.Fprintln(file, command)
-				}
-			}
-			hist.lastAppendedIndex = hist.Len()
-			return
-
-		}
-	}
-
-	if hist.Len() == 0 {
-		fmt.Fprintln(os.Stdout, "No commands in history.")
-	} else {
-
-		if cnt != 0 {
-			cnt = hist.Len() - cnt
-		}
-
-		for i := cnt; i < hist.Len(); i++ {
-			if command, exists := hist.Get(i); exists {
-				fmt.Fprintf(os.Stdout, "    %d %s\n", i+1, command)
-			}
+		} else {
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd)
 		}
 	}
 }
@@ -436,4 +203,59 @@ func getPathExecutables() []string {
 		}
 	}
 	return executables
+}
+
+func splitWithQuoting(inputString string) (string, []string) {
+	var current strings.Builder
+	args := []string{}
+	inSingleQuote := false
+	inDoubleQuote := false
+	escaped := false
+
+	for _, c := range inputString {
+		switch {
+		case escaped:
+			if inDoubleQuote {
+				switch c {
+				case '"', '\\', '$', '`':
+					current.WriteRune(c)
+				default:
+					current.WriteRune('\\')
+					current.WriteRune(c)
+				}
+			} else if !inSingleQuote {
+				current.WriteRune(c)
+			} else {
+				current.WriteRune('\\')
+				current.WriteRune(c)
+			}
+			escaped = false
+		case c == '\\' && !inSingleQuote:
+			escaped = true
+		case c == '\'' && !inDoubleQuote:
+			inSingleQuote = !inSingleQuote
+		case c == '"' && !inSingleQuote:
+			inDoubleQuote = !inDoubleQuote
+		case unicode.IsSpace(c) && !inSingleQuote && !inDoubleQuote:
+			if current.Len() > 0 {
+				args = append(args, current.String())
+				current.Reset()
+			}
+		default:
+			current.WriteRune(c)
+		}
+	}
+
+	if escaped {
+		current.WriteRune('\\')
+	}
+	if current.Len() > 0 {
+		args = append(args, current.String())
+	}
+
+	if len(args) == 0 {
+		return "", []string{}
+	}
+
+	return args[0], args
 }
--- app/utils.go
+++ app/utils.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -17,96 +20,237 @@ func isBuiltin(cmd string) bool {
 	return false
 }
 
-
-// cmdName1 and cmdName2 are the names of the commands to be executed
-// argv1 and argv2 are the arguments for the commands
-// cmd1 and cmd2 are the actual command objects created using exec.Command
 func HandlePipe(input string) {
 	parts := strings.SplitN(input, "|", 2)
 	left := strings.TrimSpace(parts[0])
 	right := strings.TrimSpace(parts[1])
 
-	// cmd1Name, cmd1Args := splitWithQuoting(left)
-	// cmd2Name, cmd2Args := splitWithQuoting(right)
-
-	// Move(cmd1Name)
-	// Move(cmd2Name)
-
-	executePipeline(strings.Split(left, " "), strings.Split(right, " "))
+	err := executePipelineBuiltinAware(strings.Split(left, " "), strings.Split(right, " "))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error executing pipeline: %s\n", err)
+		return
+	}
 }
 
-func executePipeline(cmd1Args []string, cmd2Args []string) error {
-	filePath1, exists1 := findBinInPath(cmd1Args[0])
-	if !exists1 {
-		return fmt.Errorf("%s: command not found", cmd1Args[0])
-	}
-	filePath2, exists2 := findBinInPath(cmd2Args[0])
-	if !exists2 {
-		return fmt.Errorf("%s: command not found", cmd2Args[0])
-	}
 
-	cmd1 := exec.Command(filePath1, cmd1Args[1:]...)
-	cmd2 := exec.Command(filePath2, cmd2Args[1:]...)
-
-	r, w := io.Pipe()
-	cmd1.Stdout = w
-	cmd2.Stdin = r
-	cmd2.Stdout = os.Stdout
-	cmd2.Stderr = os.Stderr
-
-	if err := cmd1.Start(); err != nil {
-		return err
-	}
-	if err := cmd2.Start(); err != nil {
-		return err
-	}
-
-	go func() {
-		cmd1.Wait()
-		w.Close()
-	}()
-
-	return cmd2.Wait()
-}
-
-// Pipeline handler that supports builtins on the left
+// Pipeline handler that supports builtins on both sides
 func executePipelineBuiltinAware(cmd1Args []string, cmd2Args []string) error {
 	r, w := io.Pipe()
 
-	if isBuiltin(cmd1Args[0]) {
-		go func() {
-			switch cmd1Args[0] {
-			case "echo":
-				EchoCommand(cmd1Args)
-				// Add other builtins here as needed
-			}
-			w.Close()
-		}()
-	} else {
-		filePath1, exists1 := findBinInPath(cmd1Args[0])
-		if !exists1 {
-			return fmt.Errorf("%s: command not found", cmd1Args[0])
-		}
-		cmd1 := exec.Command(filePath1, cmd1Args[1:]...)
-		cmd1.Stdout = w
-		cmd1.Stderr = os.Stderr
-		if err := cmd1.Start(); err != nil {
-			w.Close()
-			return err
-		}
-		go func() {
-			cmd1.Wait()
-			w.Close()
-		}()
-	}
+	// Left side
+	leftIsBuiltin := isBuiltin(cmd1Args[0])
+	// Right side
+	rightIsBuiltin := isBuiltin(cmd2Args[0])
 
-	filePath2, exists2 := findBinInPath(cmd2Args[0])
-	if !exists2 {
-		return fmt.Errorf("%s: command not found", cmd2Args[0])
+	errChan := make(chan error, 2)
+
+	// LEFT
+	go func() {
+		if leftIsBuiltin {
+			// Call builtin with w as output
+			callBuiltin(cmd1Args, os.Stdin, w)
+			w.Close()
+			errChan <- nil
+		} else {
+			filePath1, exists1 := findBinInPath(cmd1Args[0])
+			if !exists1 {
+				w.Close()
+				errChan <- fmt.Errorf("%s: command not found", cmd1Args[0])
+				return
+			}
+			cmd1 := exec.Command(filePath1, cmd1Args[1:]...)
+			cmd1.Stdout = w
+			cmd1.Stderr = os.Stderr
+			cmd1.Stdin = os.Stdin
+			err := cmd1.Run()
+			w.Close()
+			errChan <- err
+		}
+	}()
+
+	// RIGHT
+	if rightIsBuiltin {
+		callBuiltin(cmd2Args, r, os.Stdout)
+		io.Copy(io.Discard, r) // Drain the pipe to avoid deadlock
+		return <-errChan
+	} else {
+		filePath2, exists2 := findBinInPath(cmd2Args[0])
+		if !exists2 {
+			return fmt.Errorf("%s: command not found", cmd2Args[0])
+		}
+		cmd2 := exec.Command(filePath2, cmd2Args[1:]...)
+		cmd2.Stdin = r
+		cmd2.Stdout = os.Stdout
+		cmd2.Stderr = os.Stderr
+		err := cmd2.Run()
+		return err
 	}
-	cmd2 := exec.Command(filePath2, cmd2Args[1:]...)
-	cmd2.Stdin = r
-	cmd2.Stdout = os.Stdout
-	cmd2.Stderr = os.Stderr
-	return cmd2.Run()
+}
+
+// Helper to call a builtin by name and argv
+func callBuiltin(argv []string, in io.Reader, out io.Writer) {
+	switch argv[0] {
+	case "exit":
+		ExitCommand(argv, in, out, &hist)
+	case "echo":
+		EchoCommand(argv, in, out)
+	case "type":
+		TypeCommand(argv, in, out)
+	case "pwd":
+		getCurrentDir(argv, in, out)
+	case "cd":
+		changeDir(argv, in, out)
+	case "history":
+		HistoryCommand(argv, in, out, &hist)
+	}
+}
+
+func ExitCommand(argv []string, in io.Reader, out io.Writer, hist *History) {
+	code := 0
+	if len(argv) > 1 {
+		argCode, err := strconv.Atoi(argv[1])
+		if err == nil {
+			code = argCode
+		}
+	}
+	temp := "history -w " + histFile
+	HistoryCommand(strings.Split(temp, " "), in, out, hist)
+	os.Exit(code)
+}
+
+func EchoCommand(argv []string, in io.Reader, out io.Writer) {
+	if len(argv) < 2 {
+		fmt.Fprintln(out, "")
+		return
+	}
+	output := strings.Join(argv[1:], " ")
+	fmt.Fprintf(out, "%s\n", output)
+}
+
+func TypeCommand(argv []string, in io.Reader, out io.Writer) {
+	if len(argv) == 1 {
+		return
+	}
+	value := argv[1]
+	if slices.Contains(builtIns, value) {
+		fmt.Fprintf(out, "%s is a shell builtin\n", value)
+		return
+	}
+	if file, exists := findBinInPath(value); exists {
+		fmt.Fprintf(out, "%s is %s\n", value, file)
+		return
+	}
+	fmt.Fprintf(out, "%s: not found\n", value)
+}
+
+func getCurrentDir(argv []string, in io.Reader, out io.Writer) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(out, "Error getting current directory: %s\n", err)
+	}
+	fmt.Fprintf(out, "%s\n", currentDir)
+}
+
+func changeDir(argv []string, in io.Reader, out io.Writer) {
+	if len(argv) < 2 {
+		fmt.Fprintf(out, "cd: missing argument\n")
+		return
+	}
+	path := argv[1]
+	if path == "~" || path == "$HOME" {
+		path = os.Getenv("HOME")
+	}
+	if err := os.Chdir(path); err != nil {
+		fmt.Fprintf(out, "cd: %s: No such file or directory\n", path)
+	}
+}
+
+func HistoryCommand(argv []string, in io.Reader, out io.Writer, hist *History) {
+	cnt := 0
+	if len(argv) > 1 {
+		argCode, err := strconv.Atoi(argv[1])
+		if err == nil {
+			cnt = argCode
+		}
+	}
+	if len(argv) > 2 {
+		if argv[1] == "-r" {
+			if _, err := os.Stat(argv[2]); err == nil && !os.IsNotExist(err) {
+				file, err := os.ReadFile(argv[2])
+				if err != nil {
+					fmt.Fprintf(out, "Error reading history file: %s\n", err)
+					return
+				}
+				lines := strings.Split(string(file), "\n")
+				for _, line := range lines {
+					if line != "" {
+						hist.Add(line)
+					}
+				}
+				return
+			} else {
+				return
+			}
+		}
+		if argv[1] == "-w" {
+			if len(argv) < 3 {
+				fmt.Fprintln(out, "Usage: history -w <filename>")
+				return
+			}
+			file, err := os.Create(argv[2])
+			if err != nil {
+				return
+			}
+			defer file.Close()
+			for i := 1; i <= hist.Len(); i++ {
+				if command, exists := hist.Get(i - 1); exists {
+					fmt.Fprintf(file, "%s\n", command)
+				}
+			}
+			return
+		}
+		if argv[1] == "-a" {
+			if len(argv) < 3 {
+				fmt.Fprintln(out, "Usage: history -a <filename>")
+				return
+			}
+			file, err := os.OpenFile(argv[2], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Fprintf(out, "Error opening history file: %s\n", err)
+				return
+			}
+			defer file.Close()
+			for i := hist.lastAppendedIndex; i < hist.Len(); i++ {
+				if command, exists := hist.Get(i); exists {
+					fmt.Fprintln(file, command)
+				}
+			}
+			hist.lastAppendedIndex = hist.Len()
+			return
+		}
+	}
+	if hist.Len() == 0 {
+		fmt.Fprintln(out, "No commands in history.")
+	} else {
+		if cnt != 0 {
+			cnt = hist.Len() - cnt
+		}
+		for i := cnt; i < hist.Len(); i++ {
+			if command, exists := hist.Get(i); exists {
+				fmt.Fprintf(out, "    %d %s\n", i+1, command)
+			}
+		}
+	}
+}
+
+func findBinInPath(bin string) (string, bool) {
+	paths := os.Getenv("PATH")
+	for _, path := range strings.Split(paths, ":") {
+		file := filepath.Join(path, bin)
+		info, err := os.Stat(file)
+		if err == nil && !info.IsDir() && info.Mode()&0111 != 0 {
+			return file, true
+		}
+	}
+	return "", false
 }
